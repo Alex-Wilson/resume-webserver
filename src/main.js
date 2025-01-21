9b236cb72f417ad1676be9cc5ca017bb62c8c501
Alex Wilson <alex.wilson.0425@gmail.com>
2025-01-21 02:05:26 -0600

    cert list done, mixin done, css applied

--- src/main.js
+++ src/main.js
@@ -1,25 +1,37 @@
-const express = require("express");
-const path = require("path");
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
 
 const app = express();
 const PORT = 3000;
 
 // Set Pug as the view engine
-app.set("view engine", "pug");
-app.set("views", path.join(__dirname, "../views"));
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, '../views'));
 
 // Serve static files from the "public" folder
-app.use(express.static(path.join(__dirname, "../public")));
+app.use(express.static(path.join(__dirname, '../public')));
 
 // Define routes
-app.get("/", (req, res) => {
-  res.render("index"); // Render the homepage (index.pug)
+app.get('/', (req, res) => {
+  res.render('index'); // Render the homepage (index.pug)
 });
 
-app.get("/certifications", (req, res) => {
-  res.render("certifications"); // Render the certifications.pug template
-});
+app.get('/certifications', (req, res) => {
+  const certsPath = path.join(__dirname, '../public/extras/certs.json');
 
+  // Read the JSON file for certifications
+  fs.readFile(certsPath, 'utf-8', (err, data) => {
+    if (err) {
+      console.error('Error reading certifications JSON:', err);
+      res.status(500).send('Error loading certifications data');
+      return;
+    }
+
+    const certifications = JSON.parse(data).certifications; // Parse the JSON data
+    res.render('certifications', { certifications }); // Pass the data to the template
+  });
+});
 
 // Start the server
 app.listen(PORT, () => {
